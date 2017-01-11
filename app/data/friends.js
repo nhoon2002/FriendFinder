2bff13f01cfd2d4e0bd39dd18207b94326888a9a
LAPTOP-2ALLR6GQ\nhoon <nhoon2002@ucla.edu>
2017-01-11 12:12:52 -0800

    Working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,71 @@
 var friendsArray = [
    {
       "name":"Alex",
-      "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "photo":"https://s30.postimg.org/kj1s1zrep/IMG_1699.jpg",
       "scores":[
-         5,
-         1,
-         4,
-         4,
-         5,
-         1,
-         2,
-         5,
-         4,
-         1
+         "3",
+         "5",
+         "3",
+         "4",
+         "2",
+         "1",
+         "3",
+         "2",
+         "2",
+         "4"
        ]
-   }
-];
+   },
+   {
+      "name":"Chaehwa",
+      "photo":"https://postimg.org/image/bb2f8trp3/48632473/",
+      "scores":[
+        "3",
+        "3",
+        "5",
+        "1",
+        "3",
+        "1",
+        "3",
+        "1",
+        "1",
+        "3"
+      ]
+  },
+  {
+    "name":"James",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAgCAAAAJGI3NDI0MzVhLWQ2MjUtNDU1MC1hOGU3LTQ4NmE3NDM0MGJmNw.jpg",
+    "scores":[
+      "4",
+      "2",
+      "2",
+      "1",
+      "5",
+      "4",
+      "3",
+      "1",
+      "3",
+      "1"
+    ]
+  },
+  {
+    "name":"Anna",
+    "photo":"http://scontent.cdninstagram.com/t51.2885-15/s480x480/e15/c93.0.534.534/14134772_544356575747442_2130793064_n.jpg",
+    "scores":[
+      "1",
+      "1",
+      "1",
+      "1",
+      "4",
+      "4",
+      "3",
+      "3",
+      "2",
+      "1"
+    ]
+  },
+  {"name":"Alex Kim","photo":"//lh3.googleusercontent.com/Rqrcsnw4KK-E08t3eTDIoxQ-TqPe0fcX2e0A02r71uCP7vvJmltYxiZHdqOgfCyHIW-31Q=s85","scores":["5","4","1","2","2","3","1","2","4","3"]}];
+
+
 
 
 // Note how we export the array. This makes it accessible to other files using require.
